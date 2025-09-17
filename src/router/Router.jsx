87367c38099850b0d6402afce94aa1506d91cce7
manuo1 <manuo1@free.fr>
2025-09-17 18:35:45 +0200

    Replace 404 page with CV as fallback

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Experiences from "../pages/Experiences/Experiences";
-import NotFound from "../pages/NotFound/NotFound";
 import Cv from "../pages/Cv/Cv";
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Cv />} />
       <Route path="/experiences" element={<Experiences />} />
       <Route path="/cv" element={<Cv />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="*" element={<Cv />} />
     </Routes>
   );
 }
