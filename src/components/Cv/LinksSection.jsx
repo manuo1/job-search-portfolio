95606ecede01ba7ee8a3bc92ab4116cf7bc06340
manuo1 <manuo1@free.fr>
2025-09-18 13:58:49 +0200

    Add portfolio link on cv

--- src/components/Cv/LinksSection.jsx
+++ src/components/Cv/LinksSection.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import githubLogo from "../../assets/github/github-mark-white.png";
 import linkedinLogo from "../../assets/linkedin/InBug-White.png";
+import eoLogo from "../../assets/eo-logo.png";
 import styles from "./LinksSection.module.scss";
 
 const links = [
   {
+    name: "Portfolio",
+    url: "https://www.emmanuel-oudot.fr/",
+    displayUrl: "www.emmanuel-oudot.fr",
+    logo: eoLogo,
+  },
+  {
     name: "GitHub",
     url: "https://github.com/manuo1/",
+    displayUrl: "github.com/manuo1",
     logo: githubLogo,
   },
   {
     name: "LinkedIn",
     url: "https://www.linkedin.com/in/emmanuel-oudot/",
+    displayUrl: "linkedin.com/in/emmanuel-oudot",
     logo: linkedinLogo,
   },
 ];
@@ -24,7 +33,7 @@ export default function LiensSection() {
         <div key={link.name} className={styles.contactItem}>
           <img src={link.logo} alt={link.name} className={styles.contactIcon} />
           <a href={link.url} target="_blank" rel="noopener noreferrer">
-            {link.url.replace(/^https?:\/\/(www\.)?/, "")}
+            {link.displayUrl}
           </a>
         </div>
       ))}
