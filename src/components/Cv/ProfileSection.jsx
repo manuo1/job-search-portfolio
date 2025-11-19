e02d1e5b7e3b817823a950d4a41952fa04325a73
manuo1 <manuo1@free.fr>
2025-11-19 22:01:27 +0100

    refactor(cv): update profile section content

--- src/components/Cv/ProfileSection.jsx
+++ src/components/Cv/ProfileSection.jsx
@@ -5,22 +5,18 @@ export default function ProfileSection() {
   return (
     <section className={styles.profileContainer}>
       <p className={styles.profileText}>
-        <strong>Développeur Full-Stack</strong> passionné, spécialisé en{" "}
-        <strong>Python, Django</strong> et <strong>React</strong>. Issu d'une
-        première carrière dans l'optique, j'apporte une approche{" "}
-        <strong>centrée utilisateur</strong> et une solide compréhension des{" "}
-        <strong>besoins métiers</strong>.
+        <strong>Développeur passionné</strong>, je répond depuis plus de 10 ans
+        à des problèmes concrets par des applications{" "}
+        <strong>performantes</strong>,<strong> fiables</strong> et{" "}
+        <strong>scalables</strong>.
       </p>
 
       <p className={styles.profileText}>
-        Je conçois des applications <strong>performantes</strong>,{" "}
-        <strong>fiables</strong> et <strong>scalables</strong> dans des
-        environnements <strong>techniques exigeants</strong>. Mon expertise
-        couvre la <strong>gestion de données</strong>, l'<strong>IoT</strong>,
-        l'<strong>optimisation backend</strong> et les{" "}
-        <strong>architectures robustes</strong>. J'accorde une grande importance
-        à la <strong>clarté</strong>, à l'<strong>efficacité</strong> et à la{" "}
-        <strong>collaboration</strong> entre développeurs, produit et métiers.
+        J'intègre naturellement l'<strong>IA</strong> dans mon workflow pour
+        maximiser l'efficacité et privilégie une{" "}
+        <strong>communication claire</strong> et{" "}
+        <strong>fluide entre équipes</strong> afin de garantir la réussite des
+        projets.
       </p>
     </section>
   );
